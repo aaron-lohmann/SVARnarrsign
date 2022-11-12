c59a58ae18467a5da75de6b914bea606e489051a
aaron-lohmann <aaronlohmann@mail.de>
2022-11-12 19:10:46 +0100

    create restriction_check function

---
+++ R/restriction_check.R
@@ -0,0 +1,6 @@
+
+restriction_check <- function(tradsign_setup_obj = NULL,
+                              ebr_setup_obj = NULL, 
+                              narrsign_obj = NULL){
+
+                              }
