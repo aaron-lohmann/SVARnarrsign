36e596a9d305efa25850ca21b94ccf0067ed55e8
aaron-lohmann <aaronlohmann@mail.de>
2022-11-14 16:03:41 +0100

    refactoring for publication on CRAN

--- R/ebr_accept.R
+++ R/ebr_accept.R
@@ -1,55 +1,58 @@
 #' Check whether elasticity bounds are satisfied
-#' 
-#' This function take the response of the first variable for the given horizon
-#' and divides it by the response of the second variable. Then this 
 #'
-#' @param EBR Object of ebr_setup. 
-#' @param impulses Computed impulses based on parameter draws. 
-#' @param Q An orthogonal matrix. 
-#' @param allshocknames Vector of all shocknames which are identified in the 
+#' ebr_accept takes values for IRFs which are based on current draws, computes
+#' elasticity and assesses whether the elasticity bound restriction is
+#' satisified. Note that elasticities are computed here as fractions. This
+#' approach is likely only appropriate for a limited amount of application.
+#'
+#' @param ebr Object of ebr_setup.
+#' @param impulses Computed impulses based on parameter draws.
+#' @param Q An orthogonal matrix.
+#' @param allshocknames Vector of all shocknames which are identified in the
 #' the system.
 #' @param data A data frame or tibble containg the data
 #'
 #' @return A Boolean whether elasticity bounds are fulfilled
 #'
+#' @references Diaz Ramirez (2018)
 
-ebr_accept <- function(EBR = NULL,
+ebr_accept <- function(ebr = NULL,
                        impulses = NULL,
                        Q = NULL,
                        allshocknames,
                        data = data) {
   # Exit function if no elasticity bound is to be considered
-  if (is.null(EBR)) {
+  if (is.null(ebr)) {
     check <- 1
     return(check)
   }
-  
+
 
   # Define elasticity matrix which stores which of the elasticity bounds
   # are satisfieed
-  E <- matrix(NA, nrow(EBR), 1)
+  E <- matrix(NA, nrow(ebr), 1)
 
-  for (b in 1:nrow(EBR)) {
+  for (b in 1:nrow(ebr)) {
     # Pick the column which corresponds to the shock in question
-    q <- Q[, which(allshocknames == EBR$shock_names[b]), drop = FALSE]
-    
+    q <- Q[, which(allshocknames == ebr$shock_names[b]), drop = FALSE]
+
     # compute impulse response
-    ik_1 <- impulses[EBR$horizon[b], , ] %*% q
+    ik_1 <- impulses[ebr$horizon[b], , ] %*% q
     # extract value for the first relevant variable
-    ik_1 <- ik_1[EBR$first_variable[b], ]
+    ik_1 <- ik_1[ebr$first_variable[b], ]
 
     # compute impulse response
-    ik_2 <- impulses[EBR$horizon[b], , ] %*% q
+    ik_2 <- impulses[ebr$horizon[b], , ] %*% q
     # extract value for the second relevant variable
-    ik_2 <- ik_2[EBR$second_variable[b], ]
+    ik_2 <- ik_2[ebr$second_variable[b], ]
 
-    # compute elasticity as share of response of first variable divided by the 
+    # compute elasticity as share of response of first variable divided by the
     # response of the second variable
     elasticity <- ik_1 / ik_2
-    
+
     # Check whether the computed elasticity falls into the respective bound and
     # save results as a boolean value
-    E[b, ] <- elasticity < EBR$bounds[b]
+    E[b, ] <- elasticity < ebr$bounds[b]
   }
   # Check whether all of the elasticities fall into their respective bounds
   check <- prod(E)
